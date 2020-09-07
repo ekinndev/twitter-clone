8b89d5cfdda52e67202c80df3d5fcfa7fb0ef05c
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-09-07 11:20:58 +0300

    Storybook and next init

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+ import Button from '../components/Button';
+function HomePage() {
+  return <div>
+    <h1>Welcome</h1>
+    <Button>xDDDDDDDDD</Button>
+  </div>;
+}
+
+export default HomePage;
---
+++ stories/1-Button.stories.js
@@ -0,0 +1,8 @@
+import Button from '../components/Button';
+import React from 'react';
+export default {
+  title: 'Button',
+  component: Button
+};
+
+export const ourButton = () => <Button>Ses</Button>;
