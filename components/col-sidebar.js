058a54b08afb183857c3549fb9e983a7159af41a
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-09-22 13:48:56 +0300

    Tweet modalı yapıldı

--- components/col-sidebar.js
+++ components/col-sidebar.js
@@ -4,15 +4,21 @@ import Navigation from './Navigation';
 import ThemeButton from './theme-button';
 import styles from './col-sidebar.module.css';
 import ProfileBox from './profile-box';
+import TweetModal from './tweet-modal';
 function Layout({ flat }) {
+  const [isShowModal, isShowModalSet] = React.useState(false);
+  const onModalClose = () => {
+    isShowModalSet(false);
+  };
   return (
     <div className={cn(styles.sidebar)}>
       <Navigation flat={flat}></Navigation>
       <div className={styles.tweet}>
-        <ThemeButton big full={!flat}>
+        <ThemeButton big full={!flat} onClick={() => isShowModalSet(true)}>
           {flat ? 'a' : 'Tweet'}
         </ThemeButton>
       </div>
+      {isShowModal && <TweetModal onModalClose={onModalClose} onClick={onModalClose} />}
       <div className={styles.profile}>
         <ProfileBox flat={flat} />
       </div>
